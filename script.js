ca77223d3b6f671afb905f619c22bc0a387ff20b
enegussie <enegussie@fh.org>
2023-08-25 18:49:26 +0300

    filter applied

---
+++ script.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+
+	// TODO
+	results = fruit.filter( f  => f.toLowerCase().includes(str.toLowerCase()));
+	console.log(results);
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+	//	console.log(input.value);
+	let res = search(input.value);
+
+	showSuggestions(res, input.value)
+
+}
+
+function showSuggestions(results, inputVal) {
+	// TODO
+	//remove current children
+	while (suggestions.firstChild) {
+		suggestions.removeChild(suggestions.firstChild);
+	}	
+	// Loop through the array and create list items
+	for (const item of results) {
+	const listItem = document.createElement("li");
+	listItem.textContent = item;
+	suggestions.appendChild(listItem);
+	}
+}
+
+function useSuggestion(e) {
+	// TODO
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
